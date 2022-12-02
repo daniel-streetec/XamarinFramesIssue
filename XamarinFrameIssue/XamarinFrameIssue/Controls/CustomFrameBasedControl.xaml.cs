bef456e249ab5f67cbdd2cd6beb51352dc266bcb
Daniel See <daniel.see@streetec.net>
2022-12-02 08:18:32 +0100

    initial

---
+++ XamarinFrameIssue/XamarinFrameIssue/Controls/CustomFrameBasedControl.xaml.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+using Xamarin.Forms;
+using Xamarin.Forms.Xaml;
+
+namespace XamarinFrameIssue.Controls
+{
+    [XamlCompilation(XamlCompilationOptions.Compile)]
+    public partial class CustomFrameBasedControl : ContentView
+    {
+        public CustomFrameBasedControl()
+        {
+            InitializeComponent();
+        }
+
+        public static readonly BindableProperty TitleProperty = BindableProperty.Create(
+            nameof(Title),
+            typeof(string),
+            typeof(CustomFrameBasedControl),
+            "Title");
+
+        public string Title { get => (string)GetValue(TitleProperty); set => SetValue(TitleProperty, value); }
+    }
+}
---
+++ XamarinFrameIssue/XamarinFrameIssue/MainPageViewModel.cs
@@ -0,0 +1,16 @@
+﻿using CommunityToolkit.Mvvm.ComponentModel;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace XamarinFrameIssue
+{
+    [ObservableObject]
+    public partial class MainPageViewModel 
+    {
+        #region fields
+        [ObservableProperty]
+        private bool _toggled = false;
+        #endregion
+    }
+}
